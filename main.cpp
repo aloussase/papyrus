38e24243870fa7db653754fc8ac097c858310121
Alexander Goussas <goussasalexander@gmail.com>
2025-07-12 20:49:35 -0500

    feat: parse command lines args for filename

--- main.cpp
+++ main.cpp
@@ -4,12 +4,29 @@
 
 #include "PdfBackend.h"
 
-auto main() -> int {
-  static const char *source = R"( )";
+// OPTIONS
+static char *filename;
+
+auto main(int argc, char **argv) -> int {
+  argc--;
+  argv++;
+
+  if (argc != 1) {
+    fprintf(stderr, "Missing filename argument\n");
+    return 1;
+  }
+
+  filename = *argv;
+
+  // TODO: Calculate file size in bytes.
+  char source[4096] = {0};
+  FILE *fp = fopen(filename, "r");
+  fread(source, sizeof(char), sizeof(source) / sizeof(char), fp);
 
   papy::Parser parser(source);
   auto ast = parser.parse();
 
+  // TODO: Make the backend configurable
   auto backend = std::make_unique<papy::PdfBackend>();
 
   if (!ast.empty()) {
